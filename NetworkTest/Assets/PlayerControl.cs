6a8668c80a2d27a0c0e9d67c74aa9a8c5ac11e86
JNaeder <J.naeder324@gmail.com>
2018-06-16 17:24:22 -0400

    Init

---
+++ NetworkTest/Assets/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerControl : NetworkBehaviour {
+
+
+	float h;
+
+	public float speed;
+
+	Rigidbody2D rB;
+
+	// Use this for initialization
+	void Start () {
+		//rB.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasAuthority)
+		{
+			Movement();
+		}
+	}
+
+
+
+	void Movement(){
+        h = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(h * speed * Time.deltaTime, 0, 0);
+
+		//if(Input.GetButtonDown("Jump")){
+		//	rB.AddForce(Vector2.up * 2);
+
+		//}
+
+	}
+}
---
+++ NetworkTest/Assets/PlayerObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+
+	public GameObject playerUnit;
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer == false){
+			return;
+		}
+
+		CmdSpawnPlayer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	[Command]
+	void CmdSpawnPlayer(){
+		GameObject gO = Instantiate(playerUnit);
+		NetworkServer.SpawnWithClientAuthority(gO, connectionToClient);
+	}
+}
