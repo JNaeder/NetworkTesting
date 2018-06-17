3d9687207ff126181e71fe701b20b517f6750dd1
JNaeder <j.naeder324@gmail.com>
2018-06-16 22:09:41 -0400

    Updated

--- NetworkTest/Assets/PlayerControl.cs
+++ NetworkTest/Assets/PlayerControl.cs
@@ -6,15 +6,28 @@ using UnityEngine.Networking;
 public class PlayerControl : NetworkBehaviour {
 
 
-	float h;
+	float h,v;
 
 	public float speed;
+    [SyncVar]
+    public string playerName = "blank";
+    [SyncVar(hook ="UpdateHealth")]
+    public int health = 5;
+
+    public TextMesh titleName, healthNumber;
+    [SyncVar]
+    public Color startColor;
 
 	Rigidbody2D rB;
+    SpriteRenderer sP;
 
 	// Use this for initialization
 	void Start () {
-		//rB.GetComponent<Rigidbody2D>();
+        //rB.GetComponent<Rigidbody2D>();
+        sP = GetComponent<SpriteRenderer>();
+        sP.color = startColor;
+        titleName.text = playerName;
+        print(playerName);
 	}
 	
 	// Update is called once per frame
@@ -22,19 +35,37 @@ public class PlayerControl : NetworkBehaviour {
 		if (hasAuthority)
 		{
 			Movement();
-		}
-	}
 
+		}
+        healthNumber.text = health.ToString();
+
+    }
+
+
+    void UpdateHealth(int newHealth) {
+        health = newHealth;
+    }
 
 
 	void Movement(){
         h = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(h * speed * Time.deltaTime, 0, 0);
-
-		//if(Input.GetButtonDown("Jump")){
-		//	rB.AddForce(Vector2.up * 2);
-
-		//}
+        v = Input.GetAxis("Vertical");
+        transform.position += new Vector3(h * speed * Time.deltaTime, v * Time.deltaTime * speed, 0);
+        
 
 	}
+
+    [Command]
+    void CmdTakeDamage() {
+        health--;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasAuthority)
+        {
+            CmdTakeDamage();
+        }
+    }
 }
--- NetworkTest/Assets/PlayerObject.cs
+++ NetworkTest/Assets/PlayerObject.cs
@@ -7,24 +7,69 @@ public class PlayerObject : NetworkBehaviour {
 
 	public GameObject playerUnit;
 
+    [SyncVar]
+    public string playerName = "Blank";
+
+
+    Vector2 spawnPos;
+    BallSpawner ballSpawner;
+
 	// Use this for initialization
 	void Start () {
-		if(isLocalPlayer == false){
+        ballSpawner = FindObjectOfType<BallSpawner>();
+        if (isLocalPlayer == false){
 			return;
 		}
-
-		CmdSpawnPlayer();
+        
+        string n = "Dr_Monkfish" + Random.Range(1, 100);
+        playerName = n;
+        Debug.Log("I am " + playerName);
+        CmdSpawnPlayer(playerName);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isLocalPlayer){
+            return;
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+
+            CmdSpawnBalls();
+            
+
+        }
 		
+
+
 	}
+    
 
 
 	[Command]
-	void CmdSpawnPlayer(){
-		GameObject gO = Instantiate(playerUnit);
-		NetworkServer.SpawnWithClientAuthority(gO, connectionToClient);
-	}
+	void CmdSpawnPlayer(string n){
+        playerName = n;
+        Debug.Log(playerName + " has entered Server");
+        spawnPos = new Vector2(Random.Range(-7,7), 0);
+        Color randColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        GameObject gO = Instantiate(playerUnit, spawnPos, Quaternion.identity);
+        PlayerControl pC = gO.GetComponent<PlayerControl>();
+        pC.startColor = randColor;
+        pC.playerName = playerName;
+        NetworkServer.SpawnWithClientAuthority(gO, connectionToClient);
+        
+    }
+
+
+    [Command]
+    void CmdSpawnBalls() {
+
+        ballSpawner.SpawnRandomBall();
+        print("SpawnBalls");
+    }
+
+
+    
 }
